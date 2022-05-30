a90b199f4e1d860841e692aa6ab05008d65f5752
lukasbeckercode <lukas.becker.lb@outlook.com>
2022-05-30 18:29:10 +0200

    setup script checks go installation

--- setup.sh
+++ setup.sh
@@ -1,1 +1,12 @@
+echo "Looking for GoLang installation..."
+if [[ $(go version) == *"go version"* ]]; then
+  echo "GoLang already installed!"
+  go version
+else
+  #download go
+  echo "Getting GoLang from apt"
+  sudo apt-get install golang
+fi
+
+echo "install dependency for REST API"
 go get github.com/gin-gonic/gin
